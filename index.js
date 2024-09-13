6fc3db643415747976ceda1ee4098b5c5f437363
talles <talles-guerra@hotmail.com>
2024-09-13 14:02:20 +0100

    bug checked/unchecked » Resolved

--- index.js
+++ index.js
@@ -27,15 +27,15 @@ const listarMetas = async () => {
         instructions: false,
     })
 
-    if(respostas.length == 0){
-        console.log("Nenhuma meta selecionada!")
-        return
-    }
-
     metas.forEach((qualquerMeta) => {
         qualquerMeta.checked = false
     })
 
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+    
     respostas.forEach((resposta) => {
         const meta = metas.find((qualquerMeta) => {
             return qualquerMeta.value == resposta
